df996862891cbbada8db12f28e77cf78f659e889
NterpriseCEO <nuzumalex@gmail.com>
2024-10-20 23:43:34 +0100

    V1 of info panel descriptions

--- src/app/components/info-panel/info-panel.component.ts
+++ src/app/components/info-panel/info-panel.component.ts
@@ -1,19 +1,41 @@
-import { Component, EventEmitter, Input, Output } from "@angular/core";
+import { AfterViewInit, Component, EventEmitter, Input, OnDestroy, Output } from "@angular/core";
+import panels from "./panel-descriptors.json";
+import { fromEvent, Subscription } from "rxjs";
 
 @Component({
 	selector: "app-info-panel",
 	templateUrl: "./info-panel.component.html",
 	styleUrls: ["./info-panel.component.scss"]
 })
-export class InfoPanelComponent {
+export class InfoPanelComponent implements AfterViewInit, OnDestroy{
 
 	@Input() isVisible: boolean = true;
 	@Output() isVisibleChange = new EventEmitter<boolean>();
 
-	constructor() { }
+	title: string = panels[0].title;
+	description: string = panels[0].description;
+
+	hoverEvents: Subscription[] = [];
+
+	constructor() {}
+
+	ngAfterViewInit() {
+		panels.forEach(panel => {
+			console.log(panel.selector);
+			
+			this.hoverEvents.push(fromEvent(document.querySelectorAll(panel.selector), "mouseover").subscribe(() => {
+				this.title = panel.title;
+				this.description = panel.description;
+			}));
+		});
+	}
 
 	toggleInfoPanel() {
 		this.isVisible = !this.isVisible;
 		this.isVisibleChange.emit(this.isVisible);
+	}
+
+	ngOnDestroy() {
+		this.hoverEvents.forEach(event => event.unsubscribe());
 	}
 }
